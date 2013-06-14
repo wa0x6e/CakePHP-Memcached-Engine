ddb498c9350f9974db99657306c44e2ee206e6ed
Shuntaro Ogawa <shuntaro.ogawa@gmail.com>
2013-06-15 00:25:01 +0900

    add comment.

--- AmazonElastiCacheEngine.php
+++ AmazonElastiCacheEngine.php
@@ -3,6 +3,7 @@ App::uses('MemcachedEngine', 'Cache/Engine');
 /**
  * Amazon ElastiCache storage engine for cache
  *
+ * - Supports auto-discovery of the Amazon ElastiCache.
  * ex) app/Config/bootstrap.php
  * Cache::config('default', array(
  *     'engine' => 'AmazonElastiCache', //[required]
@@ -12,7 +13,8 @@ App::uses('MemcachedEngine', 'Cache/Engine');
  *      ), //[required]
  * ));
  *
- * @link          http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html#AutoDiscovery.ModifyAppPHP
+ * @link          http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/Appendix.PHPAutoDiscoverySetup.html
+ * @link          http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html
  * @package       Cake.Cache.Engine
  */
 class AmazonElastiCacheEngine extends MemcachedEngine
